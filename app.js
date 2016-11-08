d1ff9d2717fbc8a3d4108f4b8051d7d8984a439c
Aleksander-Bartosz <alex312@op.pl>
2016-11-08 19:59:39 +0100

    'up'

---
+++ app.js
@@ -0,0 +1,38 @@
+"use strict";
+document.addEventListener('DOMContentLoaded', function () {
+   
+
+   $("body > header > div > ul > li:nth-child(1)").click(function() {
+    $('html, body').animate({
+        scrollTop: $("body > nav > div > div > div").offset().top
+    }, 1500);
+    });
+    
+    $("body > header > div > ul > li:nth-child(2)").click(function() {
+    $('html, body').animate({
+        scrollTop: $("body > article > div > h1").offset().top
+    }, 1500);
+    });
+    
+    $("body > header > div > ul > li:nth-child(3)").click(function() {
+    $('html, body').animate({
+        scrollTop: $("body > section > div > div.firstSectionText > h1").offset().top
+    }, 1500);
+    });  
+    
+    $("body > header > div > ul > li:nth-child(4)").click(function() {
+    $('html, body').animate({
+        scrollTop: $("body > aside > div:nth-child(1) > div > h1").offset().top
+    }, 1500);
+    });
+    
+    $("body > header > div > ul > li:nth-child(5)").click(function() {
+    $('html, body').animate({
+        scrollTop: $("body > aside > footer > div > div.cookietext > h1").offset().top
+    }, 1500);
+    }); 
+          
+       
+       
+
+});

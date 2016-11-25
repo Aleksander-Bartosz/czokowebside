e7a03cf9a91081d73f4cc8657cdcfaa3831854f6
Aleksander-Bartosz <alex312@op.pl>
2016-11-25 18:26:30 +0100

    'smallUp'

--- README.md
+++
@@ -1,5 +0,0 @@
-# czokowebside
-
-html/css/js //8.11.2016
-
-rwd  add soon
--- app.js
+++ app.js
@@ -2,37 +2,49 @@
 document.addEventListener('DOMContentLoaded', function () {
    
 
-   $("body > header > div > ul > li:nth-child(1)").click(function() {
+   $(".menu > ul > li:nth-child(1)").click(function() {
     $('html, body').animate({
         scrollTop: $("body > nav > div > div > div").offset().top
     }, 1500);
     });
     
-    $("body > header > div > ul > li:nth-child(2)").click(function() {
+    $(".menu > ul > li:nth-child(2)").click(function() {
     $('html, body').animate({
         scrollTop: $("body > article > div > h1").offset().top
     }, 1500);
     });
     
-    $("body > header > div > ul > li:nth-child(3)").click(function() {
+    $(".menu > ul > li:nth-child(3)").click(function() {
     $('html, body').animate({
         scrollTop: $("body > section > div > div.firstSectionText > h1").offset().top
     }, 1500);
     });  
     
-    $("body > header > div > ul > li:nth-child(4)").click(function() {
+    $(".menu > ul > li:nth-child(4)").click(function() {
     $('html, body').animate({
         scrollTop: $("body > aside > div:nth-child(1) > div > h1").offset().top
     }, 1500);
     });
     
-    $("body > header > div > ul > li:nth-child(5)").click(function() {
+    $(".menu > ul > li:nth-child(5)").click(function() {
     $('html, body').animate({
-        scrollTop: $("body > footer > div > div.cookietext > h1").offset().top
-    }, 1500);
+        scrollTop: $("body > aside > footer > div > div.cookietext > h1").offset().top
+    }, 1100);
     }); 
-          
-       
+    $(window).on('resize', function () {
+        var wid = $(this).width();
+       console.log(wid); 
+    });      
+    $('#nav-icon3').click(function(){
+        
+		$(this).toggleClass('open');
+        if ($(this).hasClass("open")) {
+            $('.menu ul').css({'width':'25%','display':'block'});
+        }
+        else {
+             $('.menu ul').css('display','none');
+        }
+	});   
        
 
 });

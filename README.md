973f16b8b7e61eaf61bb953b478810aef457b81e
Aleksander-Bartosz <alex312@op.pl>
2016-12-11 11:18:03 +0100

    'pr'

--- README.md
+++
@@ -1,5 +0,0 @@
-# czokowebside
-html/css/js
-https://aleksander-bartosz.github.io/czokowebside/
-
-rwd add soon
--- app.js
+++ app.js
@@ -1,38 +1,38 @@
 "use strict";
 document.addEventListener('DOMContentLoaded', function () {
-   
+  var heigth = $('header').height();
 
    $(".menu > ul > li:nth-child(1)").click(function() {
     $('html, body').animate({
-        scrollTop: $("body > nav > div > div > div").offset().top
+        scrollTop: $("body > nav").offset().top-heigth
     }, 1500);
     });
     
     $(".menu > ul > li:nth-child(2)").click(function() {
     $('html, body').animate({
-        scrollTop: $("body > article > div > h1").offset().top
+        scrollTop: $("body > article > div > h1").offset().top-heigth
     }, 1500);
     });
     
     $(".menu > ul > li:nth-child(3)").click(function() {
     $('html, body').animate({
-        scrollTop: $("body > section > div > div.firstSectionText > h1").offset().top
+        scrollTop: $("body > section > div > div.firstSectionText > h1").offset().top-heigth
     }, 1500);
     });  
     
     $(".menu > ul > li:nth-child(4)").click(function() {
     $('html, body').animate({
-        scrollTop: $("body > aside > div:nth-child(1) > div > h1").offset().top
+        scrollTop: $("body > aside > div:nth-child(1) > div > h1").offset().top-heigth
     }, 1500);
     });
     
     $(".menu > ul > li:nth-child(5)").click(function() {
     $('html, body').animate({
-        scrollTop: $("body > aside > footer > div > div.cookietext > h1").offset().top
+        scrollTop: $("body > footer > div > div.downFooter").offset().top-heigth
     }, 1100);
     }); 
     $(window).on('resize', function () {
-        var wid = $(this).width();
+        var wid = $(this).outerWidth();
        console.log(wid); 
     });      
     $('#nav-icon3').click(function(){
@@ -45,6 +45,6 @@ document.addEventListener('DOMContentLoaded', function () {
              $('.menu ul').css('display','none');
         }
 	});   
-       
+      
 
 });
